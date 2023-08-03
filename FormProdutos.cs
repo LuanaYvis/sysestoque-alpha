e8e3b4d5a7bbe5bf5df19b810badb2014a6a598c
neilagatinha <neila48377036@edu.pa.senac.br>
2023-08-03 17:42:29 -0300

    propriedade datagrid

--- FormProdutos.cs
+++ FormProdutos.cs
@@ -9,19 +9,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace sysestoque_alpha{
-    public partial class FormProdutos : Form{
-        ICollection<Produto> Produto = new List<Produto>(); 
+namespace sysestoque_alpha
+{
+    public partial class FormProdutos : Form
+    {
+        ICollection<Produto> Produto = new List<Produto>();
         BindingSource BindingSourceProduto = new BindingSource();
 
         public FormProdutos()
         {
             InitializeComponent();
 
-            using (var db = new EstoqueContext()){
+            using (var db = new EstoqueContext())
+            {
 
                 Produto = db.Produto.ToList();
-                
+
                 BindingSourceProduto.DataSource = Produto;
 
                 teladedados.DataSource = BindingSourceProduto;
