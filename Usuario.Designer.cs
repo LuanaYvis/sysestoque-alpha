beb0c00ded2cec037686f74b02c1506958e8eeea
luanayvis <luana46922396@edu.pa.senac.br>
2023-08-08 18:01:24 -0300

    Criação do formulário de cadastro de usuários

---
+++ Usuario.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace sysestoque_alpha
+{
+    partial class Usuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            dgvUsuario = new DataGridView();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox6 = new TextBox();
+            label4 = new Label();
+            groupBox2 = new GroupBox();
+            txtEmail = new TextBox();
+            txtNome = new TextBox();
+            txtTelefone = new TextBox();
+            txtSenha = new TextBox();
+            txtLogin = new TextBox();
+            lblSenha = new Label();
+            lblNome = new Label();
+            lblTelefone = new Label();
+            lblLogin = new Label();
+            lblEmail = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvUsuario).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(470, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dgvUsuario
+            // 
+            dgvUsuario.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvUsuario.Location = new Point(12, 246);
+            dgvUsuario.Name = "dgvUsuario";
+            dgvUsuario.RowTemplate.Height = 25;
+            dgvUsuario.Size = new Size(776, 150);
+            dgvUsuario.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(551, 415);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 0;
+            button2.Text = "button1";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(632, 415);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 0;
+            button3.Text = "button1";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button1_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(713, 415);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 0;
+            button4.Text = "button1";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 1);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(776, 229);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(33, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(77, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(77, 162);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(433, 162);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(414, 37);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(370, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 0;
+            label2.Text = "label1";
+            label2.Click += label1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(33, 165);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 0;
+            label3.Text = "label1";
+            label3.Click += label1_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(77, 97);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(331, 23);
+            textBox6.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(33, 100);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 0;
+            label4.Text = "label1";
+            label4.Click += label1_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(txtEmail);
+            groupBox2.Controls.Add(txtNome);
+            groupBox2.Controls.Add(txtTelefone);
+            groupBox2.Controls.Add(txtSenha);
+            groupBox2.Controls.Add(txtLogin);
+            groupBox2.Controls.Add(lblEmail);
+            groupBox2.Controls.Add(lblSenha);
+            groupBox2.Controls.Add(lblNome);
+            groupBox2.Controls.Add(lblTelefone);
+            groupBox2.Controls.Add(lblLogin);
+            groupBox2.Location = new Point(12, 1);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(776, 229);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Dados do Usuário";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(414, 162);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(200, 23);
+            txtEmail.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(90, 97);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(331, 23);
+            txtNome.TabIndex = 1;
+            // 
+            // txtTelefone
+            // 
+            txtTelefone.Location = new Point(90, 162);
+            txtTelefone.Name = "txtTelefone";
+            txtTelefone.Size = new Size(100, 23);
+            txtTelefone.TabIndex = 1;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(442, 37);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(200, 23);
+            txtSenha.TabIndex = 1;
+            // 
+            // txtLogin
+            // 
+            txtLogin.Location = new Point(90, 37);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(200, 23);
+            txtLogin.TabIndex = 1;
+            // 
+            // lblSenha
+            // 
+            lblSenha.AutoSize = true;
+            lblSenha.Location = new Point(370, 40);
+            lblSenha.Name = "lblSenha";
+            lblSenha.Size = new Size(66, 15);
+            lblSenha.TabIndex = 0;
+            lblSenha.Text = "HashSenha";
+            lblSenha.Click += label1_Click;
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(33, 100);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(40, 15);
+            lblNome.TabIndex = 0;
+            lblNome.Text = "Nome";
+            lblNome.Click += label1_Click;
+            // 
+            // lblTelefone
+            // 
+            lblTelefone.AutoSize = true;
+            lblTelefone.Location = new Point(33, 165);
+            lblTelefone.Name = "lblTelefone";
+            lblTelefone.Size = new Size(51, 15);
+            lblTelefone.TabIndex = 0;
+            lblTelefone.Text = "Telefone";
+            lblTelefone.Click += label1_Click;
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Location = new Point(33, 40);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(37, 15);
+            lblLogin.TabIndex = 0;
+            lblLogin.Text = "Login";
+            lblLogin.Click += label1_Click;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(370, 165);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(36, 15);
+            lblEmail.TabIndex = 0;
+            lblEmail.Text = "Email";
+            lblEmail.Click += label1_Click;
+            // 
+            // Usuario
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(dgvUsuario);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Usuario";
+            Text = "Inserção de Usuário";
+            ((System.ComponentModel.ISupportInitialize)dgvUsuario).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private DataGridView dgvUsuario;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private GroupBox groupBox1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox6;
+        private Label label3;
+        private Label label4;
+        private GroupBox groupBox2;
+        private TextBox txtEmail;
+        private TextBox txtNome;
+        private TextBox txtTelefone;
+        private TextBox txtSenha;
+        private TextBox txtLogin;
+        private Label lblEmail;
+        private Label lblSenha;
+        private Label lblNome;
+        private Label lblTelefone;
+        private Label lblLogin;
+    }
+}
