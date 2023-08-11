c126e3acb0fa778bbbbca68e1b3166a99b18caeb
luanayvis <luana46922396@edu.pa.senac.br>
2023-08-11 16:10:29 -0300

    inserção dos botões de excluir e salvar

--- FormUsuario.Designer.cs
+++ FormUsuario.Designer.cs
@@ -32,22 +32,22 @@
             btnExcluir = new Button();
             btnSalvar = new Button();
             dgvUsuario = new DataGridView();
-            groupBox1 = new GroupBox();
-            lblLogin = new Label();
-            lblSenha = new Label();
-            lblNome = new Label();
-            lblTelefone = new Label();
-            lblEmail = new Label();
-            txtLogin = new TextBox();
-            txtSenha = new TextBox();
-            txtNome = new TextBox();
-            txtTelefone = new TextBox();
-            txtEmail = new TextBox();
             Login = new DataGridViewTextBoxColumn();
             HashSenha = new DataGridViewTextBoxColumn();
             Nome = new DataGridViewTextBoxColumn();
             Telefone = new DataGridViewTextBoxColumn();
             Email = new DataGridViewTextBoxColumn();
+            groupBox1 = new GroupBox();
+            txtEmail = new TextBox();
+            txtTelefone = new TextBox();
+            txtNome = new TextBox();
+            txtSenha = new TextBox();
+            txtLogin = new TextBox();
+            lblEmail = new Label();
+            lblTelefone = new Label();
+            lblNome = new Label();
+            lblSenha = new Label();
+            lblLogin = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvUsuario).BeginInit();
             groupBox1.SuspendLayout();
             SuspendLayout();
@@ -69,6 +69,7 @@
             btnExcluir.TabIndex = 1;
             btnExcluir.Text = "Excluir";
             btnExcluir.UseVisualStyleBackColor = true;
+            btnExcluir.Click += btnExcluir_Click;
             // 
             // btnSalvar
             // 
@@ -78,6 +79,7 @@
             btnSalvar.TabIndex = 2;
             btnSalvar.Text = "Salvar";
             btnSalvar.UseVisualStyleBackColor = true;
+            btnSalvar.Click += btnSalvar_Click;
             // 
             // dgvUsuario
             // 
@@ -89,105 +91,6 @@
             dgvUsuario.RowTemplate.Height = 25;
             dgvUsuario.Size = new Size(776, 165);
             dgvUsuario.TabIndex = 3;
-            // 
-            // groupBox1
-            // 
-            groupBox1.Controls.Add(txtEmail);
-            groupBox1.Controls.Add(txtTelefone);
-            groupBox1.Controls.Add(txtNome);
-            groupBox1.Controls.Add(txtSenha);
-            groupBox1.Controls.Add(txtLogin);
-            groupBox1.Controls.Add(lblEmail);
-            groupBox1.Controls.Add(lblTelefone);
-            groupBox1.Controls.Add(lblNome);
-            groupBox1.Controls.Add(lblSenha);
-            groupBox1.Controls.Add(lblLogin);
-            groupBox1.Location = new Point(12, 12);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(776, 199);
-            groupBox1.TabIndex = 4;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
-            // 
-            // lblLogin
-            // 
-            lblLogin.AutoSize = true;
-            lblLogin.Location = new Point(20, 29);
-            lblLogin.Name = "lblLogin";
-            lblLogin.Size = new Size(37, 15);
-            lblLogin.TabIndex = 0;
-            lblLogin.Text = "Login";
-            // 
-            // lblSenha
-            // 
-            lblSenha.AutoSize = true;
-            lblSenha.Location = new Point(378, 29);
-            lblSenha.Name = "lblSenha";
-            lblSenha.Size = new Size(66, 15);
-            lblSenha.TabIndex = 1;
-            lblSenha.Text = "HashSenha";
-            // 
-            // lblNome
-            // 
-            lblNome.AutoSize = true;
-            lblNome.Location = new Point(20, 77);
-            lblNome.Name = "lblNome";
-            lblNome.Size = new Size(40, 15);
-            lblNome.TabIndex = 2;
-            lblNome.Text = "Nome";
-            // 
-            // lblTelefone
-            // 
-            lblTelefone.AutoSize = true;
-            lblTelefone.Location = new Point(20, 130);
-            lblTelefone.Name = "lblTelefone";
-            lblTelefone.Size = new Size(51, 15);
-            lblTelefone.TabIndex = 3;
-            lblTelefone.Text = "Telefone";
-            // 
-            // lblEmail
-            // 
-            lblEmail.AutoSize = true;
-            lblEmail.Location = new Point(398, 104);
-            lblEmail.Name = "lblEmail";
-            lblEmail.Size = new Size(36, 15);
-            lblEmail.TabIndex = 4;
-            lblEmail.Text = "Email";
-            // 
-            // txtLogin
-            // 
-            txtLogin.Location = new Point(83, 26);
-            txtLogin.Name = "txtLogin";
-            txtLogin.Size = new Size(100, 23);
-            txtLogin.TabIndex = 5;
-            // 
-            // txtSenha
-            // 
-            txtSenha.Location = new Point(450, 26);
-            txtSenha.Name = "txtSenha";
-            txtSenha.Size = new Size(100, 23);
-            txtSenha.TabIndex = 6;
-            // 
-            // txtNome
-            // 
-            txtNome.Location = new Point(83, 74);
-            txtNome.Name = "txtNome";
-            txtNome.Size = new Size(100, 23);
-            txtNome.TabIndex = 7;
-            // 
-            // txtTelefone
-            // 
-            txtTelefone.Location = new Point(83, 127);
-            txtTelefone.Name = "txtTelefone";
-            txtTelefone.Size = new Size(100, 23);
-            txtTelefone.TabIndex = 8;
-            // 
-            // txtEmail
-            // 
-            txtEmail.Location = new Point(450, 101);
-            txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(100, 23);
-            txtEmail.TabIndex = 9;
             // 
             // Login
             // 
@@ -218,6 +121,105 @@
             Email.DataPropertyName = "Email";
             Email.HeaderText = "Email";
             Email.Name = "Email";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtEmail);
+            groupBox1.Controls.Add(txtTelefone);
+            groupBox1.Controls.Add(txtNome);
+            groupBox1.Controls.Add(txtSenha);
+            groupBox1.Controls.Add(txtLogin);
+            groupBox1.Controls.Add(lblEmail);
+            groupBox1.Controls.Add(lblTelefone);
+            groupBox1.Controls.Add(lblNome);
+            groupBox1.Controls.Add(lblSenha);
+            groupBox1.Controls.Add(lblLogin);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(776, 199);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(450, 101);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(100, 23);
+            txtEmail.TabIndex = 9;
+            // 
+            // txtTelefone
+            // 
+            txtTelefone.Location = new Point(83, 127);
+            txtTelefone.Name = "txtTelefone";
+            txtTelefone.Size = new Size(100, 23);
+            txtTelefone.TabIndex = 8;
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(83, 74);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(100, 23);
+            txtNome.TabIndex = 7;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(450, 26);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(100, 23);
+            txtSenha.TabIndex = 6;
+            // 
+            // txtLogin
+            // 
+            txtLogin.Location = new Point(83, 26);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(100, 23);
+            txtLogin.TabIndex = 5;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(398, 104);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(36, 15);
+            lblEmail.TabIndex = 4;
+            lblEmail.Text = "Email";
+            // 
+            // lblTelefone
+            // 
+            lblTelefone.AutoSize = true;
+            lblTelefone.Location = new Point(20, 130);
+            lblTelefone.Name = "lblTelefone";
+            lblTelefone.Size = new Size(51, 15);
+            lblTelefone.TabIndex = 3;
+            lblTelefone.Text = "Telefone";
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(20, 77);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(40, 15);
+            lblNome.TabIndex = 2;
+            lblNome.Text = "Nome";
+            // 
+            // lblSenha
+            // 
+            lblSenha.AutoSize = true;
+            lblSenha.Location = new Point(378, 29);
+            lblSenha.Name = "lblSenha";
+            lblSenha.Size = new Size(66, 15);
+            lblSenha.TabIndex = 1;
+            lblSenha.Text = "HashSenha";
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Location = new Point(20, 29);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(37, 15);
+            lblLogin.TabIndex = 0;
+            lblLogin.Text = "Login";
             // 
             // FormUsuario
             // 
--- FormUsuario.cs
+++ FormUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using sysestoque_alpha.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,58 @@ namespace sysestoque_alpha
 {
     public partial class FormUsuario : Form
     {
+        ICollection<Usuario> ListaUsuario = new List<Usuario>();
+        Usuario usuario = new Usuario();
+        BindingSource bindingSourceUsuario = new BindingSource();
+
         public FormUsuario()
         {
             InitializeComponent();
+
+            using (var db = new EstoqueContext())
+            {
+                ListaUsuario = db.Usuario.ToList();
+                bindingSourceUsuario.DataSource = ListaUsuario;
+                dgvUsuario.DataSource = bindingSourceUsuario;
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (dgvUsuario.SelectedRows.Count > 0)
+            {
+                usuario = dgvUsuario.SelectedRows[0].DataBoundItem as Usuario;
+
+                //Remove a categoria do Datagridview
+                bindingSourceUsuario.Remove(usuario);
+
+                //Remove do Bnaco de Dados
+                using (var db = new EstoqueContext())
+                {
+                    db.Usuario.Remove(usuario);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            usuario.Login = txtLogin.Text;
+            usuario.HashSenha = txtSenha.Text;
+            usuario.Nome = txtNome.Text;
+            usuario.Telefone = txtTelefone.Text;
+            usuario.Email = txtEmail.Text;
+
+            using (var db = new EstoqueContext())
+            {
+                db.Usuario.Add(usuario);
+                db.SaveChanges();
+
+                ListaUsuario = db.Usuario.ToList();
+                bindingSourceUsuario.DataSource = ListaUsuario;
+                dgvUsuario.DataSource = bindingSourceUsuario;
+                dgvUsuario.Refresh();
+            }
         }
     }
 }
