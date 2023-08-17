ec4c6b6d69c4cc8391e99bda8508ee542538b96c
luanayvis <luana46922396@edu.pa.senac.br>
2023-08-17 15:47:09 -0300

    inclusão do calculo de hash

--- FormLogin.cs
+++ FormLogin.cs
@@ -1,5 +1,7 @@
 using sysestoque_alpha.Models;
 using System.Drawing.Text;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace sysestoque_alpha
 {
@@ -21,6 +23,7 @@ namespace sysestoque_alpha
             string Senha = txbSenha.Text;
 
             Usuario usuario = null;
+
             //busca o usuario no banco de dados
             using (var db = new EstoqueContext())
             {
@@ -29,9 +32,31 @@ namespace sysestoque_alpha
 
             if (usuario != null)
             {
+                //calculo do hash -- utilizando a classe para construir nossa string
+                StringBuilder sb = new StringBuilder();
 
-                if (Senha == usuario.HashSenha)
+                using (SHA512 sha512 = SHA512.Create())
                 {
+                    //Criptografando a senha inserida pelo usuário
+                    //para comparar com a senha guardada no banco de de dados
+                    byte[] hashValue = sha512.ComputeHash(Encoding.UTF8.GetBytes(Login));
+                    foreach (byte b in hashValue) {
+                        sb.Append($"{b:x2}");
+                    }
+                }
+
+                Senha = sb.ToString();
+
+                if (usuario.HashSenha == Senha){
+                    this.Hide();
+
+                    /*
+                     * Abre a tela principal, dando acesso ao todas as outras telas
+                     * 
+                    FormaMain formaMain = new FormaMain();
+                    formaMain.Show();
+                    */
+
                     MessageBox.Show("Usuário logado com sucesso ");
                 }
                 else
